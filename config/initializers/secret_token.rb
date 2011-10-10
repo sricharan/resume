4023524da626bb9891b29478afda81dcbe49b8ed
Sricharan Sunder <sricharan92@gmail.com>
2011-10-10 23:53:53 +0530

    resume first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SricharanResume::Application.config.secret_token = '10d679c77d0c6d038826d35335cce9ad9b25f823cc51b365c3c2cd1cd2d1454ff2d3198562854ad4539f1d3e63c31563c1fb739fe9e9111d4d864dcca0efa7a4'
